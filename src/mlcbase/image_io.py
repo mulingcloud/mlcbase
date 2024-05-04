149d174ba6f3d6b62167e18155585b5e418797b4
Weiming Chen <wm_chen@yeah.net>
2024-05-05 00:43:03 +0800

    🐞 fix(image io): fix bugs and has a small change in image io
    Change
    - Support `plt` (matplotlib) backend
    - Replace the name of arguments `return_bs64` by `return_base64` in `get_image_from_url()`
    
    Fix bug
    - Fix the bug in `save_image()` which fail to save image in `base64` backend
    - Fix the bug in `get_image_from_url()` which return the wrong base64 encoded result when `return_base64` is True

--- src/mlcbase/image_io.py
+++ src/mlcbase/image_io.py
@@ -23,8 +23,9 @@ from io import BytesIO
 from pathlib import Path
 from typing import Optional, Any
 
-import cv2
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 from PIL import Image
 
 from .logger import Logger
@@ -34,7 +35,7 @@ from .misc import is_url, is_base64
 
 def get_image_from_url(url: str, 
                        save_path: Optional[str] = None,
-                       return_bs64: bool = False,
+                       return_base64: bool = False,
                        return_image: bool = False,
                        backend: str = "cv2",
                        logger: Optional[Logger] = None):
@@ -43,8 +44,8 @@ def get_image_from_url(url: str,
     Args:
         url (str)
         save_path (Optional[str], optional): Defaults to None.
-        return_bs64 (bool, optional): whether to return the image in base64 format. 
-                                      Defaults to False.
+        return_base64 (bool, optional): whether to return the image in base64 format. 
+                                        Defaults to False.
         return_image (bool, optional): whether to return the image. Defaults to False.
         backend (str, optional): backend of the image. Only used when return_image 
                                  is True. Defaults to "cv2".
@@ -60,8 +61,8 @@ def get_image_from_url(url: str,
         if logger is not None:
             logger.error(f"Invalid url: {url}")
         raise ValueError(f"Invalid url: {url}")
-    assert return_bs64 or return_image, "At least one of return_bs64 and return_image must be True"
-    assert backend in ["cv2", "pillow"], f"Invalid backend: {backend}"
+    assert return_base64 or return_image, "At least one of return_bs64 and return_image must be True"
+    assert backend in ["cv2", "pillow", "plt"], f"Invalid backend: {backend}"
     
     response = requests.get(url)
     if response.status_code == 200:
@@ -72,8 +73,9 @@ def get_image_from_url(url: str,
                 f.write(img_bs64)
         
         image_dict = ConfigDict()
-        if return_bs64:
-            image_dict.base64 = img_bs64
+        if return_base64:
+            img = base64.b64encode(img_bs64)
+            image_dict.base64 = img
 
         if return_image:
             if backend == "cv2":
@@ -86,6 +88,16 @@ def get_image_from_url(url: str,
                 img = Image.open(img)
                 image_dict.image = img
 
+            if backend == "plt":
+                img = np.fromstring(img_bs64, np.uint8)
+                img = cv2.imdecode(img, cv2.IMREAD_UNCHANGED)
+                if len(img.shape) == 3:
+                    if img.shape[2] == 3:
+                        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    if img.shape[2] == 4:
+                        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+                image_dict.image = img
+
         return image_dict
     else:
         if logger is not None:
@@ -95,15 +107,13 @@ def get_image_from_url(url: str,
 
 def load_image(path: str, 
                backend: str = "cv2", 
-               format: str = "png",
-               logger: Optional[Logger] = None):
+               logger: Optional[Logger] = None,
+               **kwargs):
     """load an image
 
     Args:
         path (str)
         backend (str, optional): backend of the output image. Defaults to "cv2".
-        format (str, optional): saving format of base64. Only used if backend is 
-                                base64. Defaults to "png".
         logger (Optional[Logger], optional): Defaults to None.
 
     Raises:
@@ -112,7 +122,7 @@ def load_image(path: str,
     Returns:
         np.ndarray or PIL.Image: depends on the backend
     """
-    assert backend in ["cv2", "pillow", "base64"], f"Invalid backend: {backend}"
+    assert backend in ["cv2", "pillow", "plt", "base64"], f"Invalid backend: {backend}"
 
     if Path(path).exists():
         is_local_path = True
@@ -125,17 +135,17 @@ def load_image(path: str,
     
     if is_local_path:
         if backend == "cv2":
-            img = cv2.imread(path)
+            img = cv2.imread(path, kwargs.get("flags", cv2.IMREAD_UNCHANGED))
         
         if backend == "pillow":
-            img = Image.open(path)
+            img = Image.open(path, formats=kwargs.get("formats", None))
+
+        if backend == "plt":
+            img = plt.imread(path, format=kwargs.get("format", None))
 
         if backend == "base64":
-            img = Image.open(path)
-            buffer = BytesIO()
-            img.save(buffer, format=format)
-            byte_data = buffer.getvalue()
-            img = base64.b64encode(byte_data)
+            with open(path, "rb") as f:
+                img = base64.b64encode(f.read())
     else:
         if backend == "base64":
             img = get_image_from_url(path, return_bs64=True, logger=logger).base64
@@ -148,7 +158,8 @@ def load_image(path: str,
 def save_image(img: Any, 
                path: str, 
                backend: str = "cv2", 
-               logger: Optional[Logger] = None):
+               logger: Optional[Logger] = None,
+               **kwargs):
     """save an image to local device
 
     Args:
@@ -160,14 +171,17 @@ def save_image(img: Any,
     Returns:
         bool: return True if success, otherwise return False
     """
-    assert backend in ["cv2", "pillow", "base64"], f"Invalid backend: {backend}"
+    assert backend in ["cv2", "pillow", "plt", "base64"], f"Invalid backend: {backend}"
     
     try:
         if backend == "cv2":
             cv2.imwrite(path, img)
         
         if backend == "pillow":
-            img.save(path)
+            img.save(path, **kwargs)
+
+        if backend == "plt":
+            plt.imsave(path, img, **kwargs)
         
         if backend == "base64":
             if not is_base64(img):
@@ -176,6 +190,7 @@ def save_image(img: Any,
                 raise TypeError("The input image is not in base64 format.")
             
             with open(path, "wb") as f:
+                img = base64.b64decode(img)
                 f.write(img)
         return True
     except OSError as e:
